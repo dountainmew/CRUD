5ea464dbc34dcf7067195ba77f8940b80ded5ccc
dountainmew <adam.david1440@gmail.com>
2018-03-06 21:40:04 +0100

    Second commit

--- src/Controller/CrudController.php
+++ src/Controller/CrudController.php
@@ -3,15 +3,14 @@ namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Doctrine\DBAL\Driver\Connection;
 
 class CrudController extends Controller
 {
-    public function number()
+    public function indexAction(Connection $connection)
     {
-        $number = mt_rand(0, 100);
-
-        return $this->render('crud/crud.html.twig', array(
-            'number' => $number,
-		));
+		$users = $connection->fetchAll('SELECT * FROM users');
+		return $this->render('crud\crud.html.twig', array('users' => $users));
     }
+	
 }
